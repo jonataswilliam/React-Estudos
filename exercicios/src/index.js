830463c75da0b193351ed5f745c9a2ac62ed93fa
Jonatas William <jonatas_2602@hotmail.com>
2018-07-15 11:03:12 -0300

    Exportando e importando multiplos componentes

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import Primeiro from './components/Primeiro'
-import BomDia from './components/BomDia'
+// import BomDia from './components/BomDia'
+import Multi, {BoaTarde, BoaNoite} from './components/Multiplos'
 
-ReactDOM.render(<BomDia nome="Jonatas" />, document.getElementById('root'))
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Jonatas" />
+        <BoaTarde nome="Ana" />
+        <BoaNoite nome="Bia" />
+    </div>
+,document.getElementById('root'))
