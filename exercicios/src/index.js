71d2ca196f4c6a2e0b404e4a10ec1f3a3c6e8902
Jonatas William <jonatas_2602@hotmail.com>
2018-07-15 11:39:52 -0300

    Componentes atraves de classe

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom'
 
 // import Primeiro from './components/Primeiro'
 // import BomDia from './components/BomDia'
-import Multi, {BoaTarde, BoaNoite} from './components/Multiplos'
+// import Multi, {BoaTarde, BoaNoite} from './components/Multiplos'
+import Saudacao from './components/Saudacao'
 
 
 ReactDOM.render(
     <div>
-        <Multi.BoaTarde nome="Jonatas" />
-        <BoaTarde nome="Ana" />
-        <BoaNoite nome="Bia" />
+        <Saudacao tipo="Bom dia" nome="João" />
     </div>
 ,document.getElementById('root'))
